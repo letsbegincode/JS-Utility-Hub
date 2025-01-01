55c2c0c0d5e54ed55256c4b98ae16eda9bbfcf2f
Abhi <134317648+letsbegincode@users.noreply.github.com>
2025-01-01 16:59:08 +0530

    Create Counter_Utility.js
    Create Counter Utility by using Closures

---
+++ Closures/Counter_Utility.js
@@ -0,0 +1,16 @@
+function createCounter() {
+  let count = 0;
+  return {
+    increment() {
+      count++;
+      return count;
+    },
+    decrement() {
+      count--;
+      return count;
+    },
+    getCount() {
+      return count;
+    }
+  };
+}
